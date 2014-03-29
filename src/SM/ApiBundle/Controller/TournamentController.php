bac4900c00890d138cc0453bb9a0ecd85f864c65
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-03-29 10:33:31 +0100

    Add jsonp handler

--- src/SM/ApiBundle/Controller/TournamentController.php
+++ src/SM/ApiBundle/Controller/TournamentController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 /**
  * @Rest\NamePrefix("sm_api_tournament_")
  */
@@ -19,7 +20,22 @@ class TournamentController extends FOSRestController
     {
     	$manager = $this->getDoctrine()->getManager();
     	$repo = $manager->getRepository("SM\ApiBundle\Entity\Tournament");
-		return array("tournaments" => $repo->findAll());
+		
+    	$callback = $this->getRequest()->get('callback');
+    	
+    	$tournaments = $repo->findAll();
+    	$data = array("tournaments" => $tournaments);
+    	
+    	if (isset($callback)) {
+    		$response = new JsonResponse();
+    		$response->setCallback($callback);
+    		$serializer = $this->container->get('jms_serializer');
+    		$serializedTournaments = $serializer->serialize($data, 'json');
+    		$response->setContent($callback . "(" . $serializedTournaments . ")");
+    		return $response;	
+    	}
+    	
+    	return $data;
     }
 
 }
